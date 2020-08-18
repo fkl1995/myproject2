2c3f3eb74cc6fce34cc242f5043bd284533aa730
fkl1995 <fklin20713@gmail.com>
2020-08-18 11:38:10 -0400

    Initial commit

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,15 @@
+package com.tom
+
+fun main() {
+//    println("Hello Kotlin")
+//    Human().hello()
+    val h= Human()
+    h.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello kotlin")
+    }
+
+}
